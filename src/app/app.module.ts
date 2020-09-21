95b6c701ba1cc6f371f9bf8e3ed822fc6f6b68a7
Kanok-GB <kanok.greatboy@gmail.com>
2020-09-21 21:28:38 +0700

    Test Use My Package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderModule } from 'mkt-packagr-test';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
